32f809f4b7b592b7809cdb96d979a0bcb0f662c4
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2020-03-16 22:24:43 +0530

    null_pointer_exception
    Exception in thread "main" java.lang.NullPointerException
            at null_pointer_exception.main(null_pointer_exception.java:5)

---
+++ null_pointer_exception.java
@@ -0,0 +1,8 @@
+class null_pointer_exception{
+	 public static void main(String[] args) {
+		String s1=null;
+		System.out.println("First line:");
+		System.out.println("String length:" + s1.length());
+		System.out.println("Last line");
+	}
+}
